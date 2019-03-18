68480c8a238027fb9c5527df46ce9b77f7996cb7
Juan Alorro <juan.alorro@flatironschool.com>
2019-03-18 14:35:21 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,34 @@
 class StudentsController < ApplicationController
-  
+
+  before_action :get_student, only: [:show]
+
   def index
     @students = Student.all
   end
 
+  def new
+
+  end
+
   def show
+    get_student
+  end
+
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to @student
+  end
+
+  private
+
+  def get_student
     @student = Student.find(params[:id])
   end
 
-  def new
-  end
-
-  def create
-  end
+  # def student_params
+  #   byebug
+  #   params.require(:student).permit(:first_name, :last_name)
+  # end
 
 end
